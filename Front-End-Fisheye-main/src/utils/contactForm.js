fb5f958af0fac006124729e2e6b5624fadccac77
neochatelin <neo.chatelin@gmail.com>
2023-01-24 14:48:11 +0100

    _

--- Front-End-Fisheye-main/src/utils/contactForm.js
+++ Front-End-Fisheye-main/src/utils/contactForm.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-unused-vars */
 function displayModal () {
   const modal = document.getElementById('contact_modal')
+  const form = document.querySelector('form')
   modal.style.display = 'block'
-  document.querySelector('.contact_button').addEventListener('click', (e) => { e.preventDefault(); console.log(e) })
+  document.querySelector('.contact_submit_button').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(form.elements[0].value)
+    console.log(form.elements[1].value)
+    console.log(form.elements[2].value)
+    console.log(form.elements[3].value)
+  })
 }
 
 function closeModal () {
